ce45c3bbe7e43ce79355411154581e6705dddef3
Eduard Gert <edefritz@gmail.com>
2023-02-10 15:04:37 +0100

    Fix get tests

--- src/test/scala/io/github/edefritz/test/commands/GetTest.scala
+++ src/test/scala/io/github/edefritz/test/commands/GetTest.scala
@@ -5,15 +5,34 @@ import cats.effect.unsafe.implicits.global
 import io.circe.Json
 import io.github.edefritz.Application.connection
 import io.github.edefritz.client.Tile38Client
-import io.github.edefritz.commands.GetCommand
+import io.github.edefritz.commands.{ GetCommand, SetCommand }
+import io.github.edefritz.commands.SetCommand.{ Point => SetPoint }
 import io.github.edefritz.responses._
 import io.github.edefritz.test.util.JsonAssertions
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AsyncFunSuite
 
+import java.util.UUID
+
 class GetTest extends AsyncFunSuite with BeforeAndAfterAll with JsonAssertions {
 
-  val client: Tile38Client[IO] = Tile38Client.forAsync[IO](connection)
+  private val client: Tile38Client[IO] = Tile38Client.forAsync[IO](connection)
+  // ARRANGE
+  private val key = UUID.randomUUID().toString
+  private val id  = UUID.randomUUID().toString
+
+  override def beforeAll(): Unit = {
+    val setCommand = SetCommand(
+      key,
+      id,
+      inputFormat = SetPoint(1, 1),
+      fields = Map("speed" -> 10)
+    )
+
+    for {
+      result <- client.exec(setCommand).unsafeToFuture()
+    } yield result
+  }
 
   test("Test get point command") {
     // ARRANGE
@@ -21,7 +40,7 @@ class GetTest extends AsyncFunSuite with BeforeAndAfterAll with JsonAssertions {
 
     // ACT
     for {
-      result <- client.exec(GetCommand("fleet", "truck1", outputFormat = GetCommand.Point)).unsafeToFuture()
+      result <- client.exec(GetCommand(key, id, outputFormat = GetCommand.Point)).unsafeToFuture()
     } yield result match {
       // ASSERT
       case PointResponse(_, _, point, _) =>
@@ -45,7 +64,7 @@ class GetTest extends AsyncFunSuite with BeforeAndAfterAll with JsonAssertions {
     val expectedObject: Json = parseOrElse(jsonString)
     // ACT
     for {
-      result <- client.exec(GetCommand("fleet", "truck1", outputFormat = GetCommand.Object)).unsafeToFuture()
+      result <- client.exec(GetCommand(key, id, outputFormat = GetCommand.Object)).unsafeToFuture()
     } yield result match {
       // ASSERT
       case o: ObjectResponse =>
@@ -60,7 +79,7 @@ class GetTest extends AsyncFunSuite with BeforeAndAfterAll with JsonAssertions {
     val expectedOutput: BoundsResponse = BoundsResponse(ok = true, "", Bounds(LatLon(1.0, 1.0), LatLon(1.0, 1.0)))
     // ACT
     for {
-      result <- client.exec(GetCommand("fleet", "truck1", outputFormat = GetCommand.Bounds)).unsafeToFuture()
+      result <- client.exec(GetCommand(key, id, outputFormat = GetCommand.Bounds)).unsafeToFuture()
     } yield result match {
       // ASSERT
       case o: BoundsResponse =>
@@ -75,7 +94,7 @@ class GetTest extends AsyncFunSuite with BeforeAndAfterAll with JsonAssertions {
     val expectedOutput: HashResponse = HashResponse(ok = true, "", "s00tw")
     // ACT
     for {
-      result <- client.exec(GetCommand("fleet", "truck1", outputFormat = GetCommand.Hash(5))).unsafeToFuture()
+      result <- client.exec(GetCommand(key, id, outputFormat = GetCommand.Hash(5))).unsafeToFuture()
     } yield result match {
       // ASSERT
       case o: HashResponse =>
@@ -90,8 +109,7 @@ class GetTest extends AsyncFunSuite with BeforeAndAfterAll with JsonAssertions {
     val expectedOutput: Map[String, Double] = Map[String, Double]("speed" -> 10)
     // ACT
     for {
-      result <-
-        client.exec(GetCommand("fleet", "truck1", withFields = true, outputFormat = GetCommand.Point)).unsafeToFuture()
+      result <- client.exec(GetCommand(key, id, withFields = true, outputFormat = GetCommand.Point)).unsafeToFuture()
     } yield result match {
       // ASSERT
       case o: PointResponse =>
